dea6e141ac7d2f4b0d7ec7f8c76613d026505361
SAyC16 <143815233+SAyC16@users.noreply.github.com>
2025-01-16 22:04:30 +0200

    Added dates and time

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElememt = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000)
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
-
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElememt.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}"class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
